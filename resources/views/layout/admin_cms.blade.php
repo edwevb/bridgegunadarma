10ebcc294ccf761ee3821ad19adc6d2e42fdae7b
Edward Evbert <edwardevbert@gmail.com>
2020-05-27 00:18:39 +0700

    initial release

--- resources/views/layout/admin_cms.blade.php
+++ resources/views/layout/admin_cms.blade.php
@@ -22,7 +22,8 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   
   {{--  Page styles --}}
-  <link rel="stylesheet" href="{{ asset('assets/css/bridgeug.css') }}">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  {{-- <link rel="stylesheet" href="{{ asset('assets/css/bridgeug.css') }}"> --}}
   <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.bridge.css') }}">
 
   <style type="text/css">
@@ -173,8 +174,10 @@
   
 
   {{-- Page scripts --}}
-  <script src="{{ asset('assets/js/bridge/bridge-jquery.js') }}"></script>
-  <script src="{{ asset('assets/js/bridge/bridge-popper.js') }}"></script>
+  {{-- <script src="{{ asset('assets/js/bridge/bridge-jquery.js') }}"></script>
+  <script src="{{ asset('assets/js/bridge/bridge-popper.js') }}"></script> --}}
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="{{ asset('assets/js/bridge/bridge.js') }}"></script>
 
   {{-- Page plugins --}}
