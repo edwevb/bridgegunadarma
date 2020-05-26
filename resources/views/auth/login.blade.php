a05a128d20aca1253471dd27d5b26cc5d8d5a565
Edward Evbert <edwardevbert@gmail.com>
2020-05-27 03:19:57 +0700

    initial release

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,10 +23,10 @@
             <form method="POST" action="{{ route('login') }}">
               @csrf
               <div class="form-group row">
-                <label for="username" class="col-md-4 col-form-label text-md-right text-white">{{ __('Username') }}</label>
+                <label for="email" class="col-md-4 col-form-label text-md-right text-white">{{ __('email') }}</label>
                 <div class="col-md-6">
-                  <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
-                  @error('username')
+                  <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                  @error('email')
                     <span class="invalid-feedback" role="alert">
                       <strong>{{ $message }}</strong>
                     </span>
