3fdf1e58edb766099ed6ac6a122e85013650a261
Edward Evbert <edwardevbert@gmail.com>
2020-05-27 16:14:11 +0700

    inital release

--- resources/views/layout/admin_cms.blade.php
+++ resources/views/layout/admin_cms.blade.php
@@ -22,7 +22,8 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   
   {{--  Page styles --}}
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
   <link rel="stylesheet" href="{{ asset('assets/css/bridgeug.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/vendor/datatables/dataTables.bootstrap4.min.css') }}">
  {{--  <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.bridge.css') }}"> --}}
@@ -175,14 +176,16 @@
   
 
   {{-- Page scripts --}}
-  {{-- <script src="{{ asset('assets/js/bridge/bridge-jquery.js') }}"></script>
-  <script src="{{ asset('assets/js/bridge/bridge-popper.js') }}"></script> --}}
+
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="{{ asset('assets/js/bridge/bridge.js') }}"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+{{--   <script src="{{ asset('assets/js/bridge/bridge-jquery.js') }}"></script>
+  <script src="{{ asset('assets/js/bridge/bridge-popper.js') }}"></script>
+  <script src="{{ asset('assets/js/bridge/bridge.js') }}"></script> --}}
 
   {{-- Page plugins --}}
-    <script src="{{ asset('assets/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/datatables/jquery.dataTables.min.js') }}"></script>
   <script src="{{ asset('assets/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
   <script src="{{ asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
   
