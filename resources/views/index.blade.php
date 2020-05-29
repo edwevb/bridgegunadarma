81311b5f1b8816f55bb286d57e159e76d4d74eb9
Edward Evbert <edwardevbert@gmail.com>
2020-05-29 17:56:47 +0700

    initial release

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,7 +49,7 @@
 					  <article id="atlet"> {{-- atlet article --}}
 						  <div class="card-borderless">
             		<div class="p-1 text-center">
-        					<a id="header-article" href="#atlet-collapse" class="text-shadow" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="atlet-collapse"><h1 class="display-4 font-weight-bold text-shadow">ATLET<i class="fa fa-fw fa-caret-down bg-gradient-success rounded-circle ml-2"></i></h1></a>
+        					<a id="header-article" href="#atlet-collapse" class="text-shadow" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="atlet-collapse"><h1 class="display-5 font-weight-bold text-shadow">ATLET<i class="fa fa-fw fa-caret-down bg-gradient-success rounded-circle ml-2"></i></h1></a>
             			<div class="collapse hide bg-transparent" id="atlet-collapse">
             				<div class="p-3">
 			                <div class="row justify-content-center row-cols-1 row-cols-lg-3" >
@@ -81,7 +81,7 @@
 						<article id="prestasi"> {{-- prestasi article --}}
 						  <div class="card-borderless">
             		<div class="p-1 text-center">
-        					<a id="header-article" href="#prestasi-collapse" class="text-shadow" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="prestasi-collapse"><h1 class="display-4 font-weight-bold text-shadow">PRESTASI<i class="fa fa-fw fa-caret-down  bg-gradient-info rounded-circle ml-2"></i></h1></a>
+        					<a id="header-article" href="#prestasi-collapse" class="text-shadow" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="prestasi-collapse"><h1 class="display-5 font-weight-bold text-shadow">PRESTASI<i class="fa fa-fw fa-caret-down  bg-gradient-info rounded-circle ml-2"></i></h1></a>
             			<div class="collapse hide bg-transparent" id="prestasi-collapse">
             				<div class="p-4">
 				            	<div class="card-borderless">
@@ -116,7 +116,7 @@
 						 <article id="event"> {{-- event article --}}
 						  <div class="card-borderless">
             		<div class="p-1 text-center">
-          				<a id="header-article" href="#event-collapse" class="text-shadow" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="event-collapse"><h1 class="display-4 font-weight-bold text-shadow">TOURNAMENT / EVENT<i class="fa fa-fw fa-caret-down  bg-gradient-warning rounded-circle ml-2"></i></h1></a>
+          				<a id="header-article" href="#event-collapse" class="text-shadow" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="event-collapse"><h1 class="display-5 font-weight-bold text-shadow">TOURNAMENT / EVENT<i class="fa fa-fw fa-caret-down  bg-gradient-warning rounded-circle ml-2"></i></h1></a>
             			<div class="collapse hide bg-transparent" id="event-collapse">
             				<div class="p-4">
 				            	<div class="card-borderless">
