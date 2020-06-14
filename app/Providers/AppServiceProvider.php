450652756462e6801a470553d48181fb5f273723
Edward Evbert <edwardevbert@gmail.com>
2020-06-14 22:40:47 +0700

    initial release

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+      if($this->app->environment('production'))
+      {
+          \URL::forceScheme('https');
+      }
 
-      \URL::forceScheme('https');
-      
       Schema::defaultStringLength(255);
 
       Validator::extend('alpha_spaces', function($attribute, $value)
