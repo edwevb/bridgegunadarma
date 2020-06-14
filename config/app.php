d58c09d238c25445ab690099236f04a3213f61b1
Edward Evbert <edwardevbert@gmail.com>
2020-06-14 22:41:22 +0700

    initial release

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://bridgegunadarma.herokuapp.com/'),
+    'url' => env('APP_URL', 'https://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
