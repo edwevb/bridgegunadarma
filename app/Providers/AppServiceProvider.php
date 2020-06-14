017446c8c2d72dc4234db5fcd4e931646c712400
Edward Evbert <edwardevbert@gmail.com>
2020-06-14 21:47:51 +0700

    initial release

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+       if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+             \URL::forceScheme('https');
+        }
+        
       Schema::defaultStringLength(255);
 
       Validator::extend('alpha_spaces', function($attribute, $value)
