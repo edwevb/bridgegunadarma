31f0a2cf85de620878ab8b714542b6ca1af047e1
Edward Evbert <edwardevbert@gmail.com>
2020-06-14 15:46:09 +0700

    initial release

--- resources/views/layout/admin_cms.blade.php
+++ resources/views/layout/admin_cms.blade.php
@@ -210,6 +210,7 @@
   @yield('section')
   <br><br><br><br><br><br><br><br><br><br>
   {{-- Page scripts --}}
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="{{ asset('js/app.js') }}" defer></script>
   {{-- Page plugins --}}
   @yield('footer')
