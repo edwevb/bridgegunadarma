2fe5e95e21266d592e28960e7b07798c951a0daf
Edward Evbert <edwardevbert@gmail.com>
2020-09-06 01:48:30 +0700

    Update

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -54,7 +54,7 @@
             @if(auth()->user())
              <li class="nav-item dropdown">
                 <a class="nav-link-style dropdown-toggle" data-toggle="dropdown" href="javascript:;" data-target="#account"><i class="far fa-user-circle"></i> {{auth()->user()->email}}</a>
-                <div class="dropdown-menu collapse" id="account">
+                <div class="dropdown-menu collapse mr-auto" id="account">
                   <a class="dropdown-item" href="{{ url('/dashboard') }}"><i class="fas fa-laptop"></i> Dashboard</a>
                   <a class="dropdown-item" href="{{ url('/passwordForm/'.auth()->user()->id) }}"><i class="fas fa-pen-alt"></i> Change Password</a>
                   @if (auth()->user()->role_id!=1)
